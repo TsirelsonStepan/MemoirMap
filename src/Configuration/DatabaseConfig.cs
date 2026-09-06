2164c803cae2c9b55246439caabc12f0ac842558
Stepan Tsirelson <tsirelsons@gmail.com>
2026-09-06 22:26:58 +0300

    Fixed null exception at database initialization

--- src/Configuration/DatabaseConfig.cs
+++ src/Configuration/DatabaseConfig.cs
@@ -6,8 +6,6 @@ public static class DatabaseConfig
 {
     public static IServiceCollection InitializeNpgsqlDatabase(this IServiceCollection services, string? connectionString)
     {
-        if (connectionString == null) throw new Exception();
-        
         services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseNpgsql(connectionString);
