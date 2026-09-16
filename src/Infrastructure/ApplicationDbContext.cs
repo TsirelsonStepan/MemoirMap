169ef0f6bed8afef3d883aa7fb33d932eaeae00d
Stepan Tsirelson <tsirelsons@gmail.com>
2026-09-16 12:01:37 +0300

    Init merge

--- src/Infrastructure/ApplicationDbContext.cs
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -6,13 +6,6 @@ namespace MemoirMap.Infrastructure;
 
 public class ApplicationDbContext : IdentityDbContext<UserAccountEntity>
 {
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-
-        builder.Entity<UserAccountEntity>().Ignore(x => x.Username);
-    }
-
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) {}
 
     //Future DbSets
--- src/Models/EntityModels/UserAccountEntity.cs
+++ src/Models/EntityModels/UserAccountEntity.cs
@@ -2,24 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MemoirMap.Models.EntityModels;
 
-public class UserAccountEntity : IdentityUser, IUserAccountEntity
+public class UserAccountEntity : IdentityUser
 {
     public UserAccountEntity() : base() {}
     public UserAccountEntity(string username) : base(username) {}
-
-    public string Username
-    {
-        get
-        {
-            return base.UserName ?? throw new ArgumentNullException();
-        }
-        set => base.UserName = value;
-    }
-}
-
-public interface IUserAccountEntity
-{
-    public string Id { get; set; }
-    public string Username { get; set; }
-    public string? PasswordHash { get; set; }
 }
